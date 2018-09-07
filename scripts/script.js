6445fbea7e412d3474d92ea052f1e883ee1cb443
Manuel Aldrin <manuelaldrinbalbino@gmail.com>
2018-09-07 15:51:38 +0800

    resi video

--- scripts/script.js
+++ scripts/script.js
@@ -105,6 +105,40 @@
 				$('.resi-contact-form #txtBackground').text(text);
 			})
 
+			$.fn.isOnScreen = function(){
+    
+			    var win = $(window);
+			    
+			    var viewport = {
+			        top : win.scrollTop(),
+			        left : win.scrollLeft()
+			    };
+			    viewport.right = viewport.left + win.width();
+			    viewport.bottom = viewport.top + win.height();
+			    
+			    var bounds = this.offset();
+			    bounds.right = bounds.left + this.outerWidth();
+			    bounds.bottom = bounds.top + this.outerHeight();
+			    
+			    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+			    
+			};
+
+			$(window).scroll(function(){
+				if($('.footer-wrap').isOnScreen() == true){
+					$('.scroll-up-wrap').fadeIn();
+					$('.scroll-down-wrap').fadeOut();
+				}else{
+					$('.scroll-up-wrap').fadeOut();
+					$('.scroll-down-wrap').fadeIn();
+				}
+			})
+
+			$('.video-main video').css({'opacity':'1'});
+			
+
+			
+
 			$.scrollify({
 				section : ".section",
 				// sectionName : "section-name",
